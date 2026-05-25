03a603dfd36d76516d43813b6c327b5238566e5c
gbakulin <galbakulina@gmail.com>
2026-05-25 17:05:55 +0200

    changed the main.go to be able to have custome metrics

--- backend/main.go
+++ backend/main.go
@@ -5,9 +5,12 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+
 	"transcendance/internal/handlers"
 	"transcendance/internal/repository"
 	"transcendance/internal/ws"
+	"transcendance/internal/metrics"
 )
 
 func main() {
@@ -19,16 +22,19 @@ func main() {
 	hub := ws.NewHub()
 	go hub.Run() // On lance le Hub en arrière-plan (goroutine)
 
+	mux := http.NewServeMux()
+
 	// 3. Déclaration des routes HTTP
-	http.HandleFunc("/api/register", authHandler.Register)
-	http.HandleFunc("/api/login", authHandler.Login)
+	mux.HandleFunc("/api/register", authHandler.Register)
+	mux.HandleFunc("/api/login", authHandler.Login)
+	mux.Handle("/metrics", promhttp.Handler())
 
 	// 4. LA NOUVELLE ROUTE WEBSOCKET
-	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		// On autorise toutes les origines pour le développement (CORS)
 		ws.ServeWs(hub, w, r)
 	})
 
 	log.Println("Serveur sur http://localhost:8081")
-	log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Fatal(http.ListenAndServe(":8081", metrics.TrackRequests(mux)))
 }
