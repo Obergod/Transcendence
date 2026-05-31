76a2a671a31ed579d1e3e3cc3d9fba41d1759c13
gbakulin <galbakulina@gmail.com>
2026-05-31 14:34:51 +0200

    grafana + prometeus + dashboards

--- backend/main.go
+++ backend/main.go
@@ -25,16 +25,24 @@ func main() {
 	mux := http.NewServeMux()
 
 	// 3. Déclaration des routes HTTP
-	mux.HandleFunc("/api/register", authHandler.Register)
-	mux.HandleFunc("/api/login", authHandler.Login)
-	mux.Handle("/metrics", promhttp.Handler())
+	// mux.HandleFunc("/api/register", authHandler.Register)
+	// mux.HandleFunc("/api/login", authHandler.Login)
+	mux.Handle("/api/register", metrics.TrackRequests(metrics.TrackActiveUsers(metrics.TrackVisitDuration(metrics.TrackUniqueVisitors(http.HandlerFunc(authHandler.Register))))))
+    mux.Handle("/api/login", metrics.TrackRequests(metrics.TrackActiveUsers(metrics.TrackVisitDuration(metrics.TrackUniqueVisitors(http.HandlerFunc(authHandler.Login))))))
+    mux.Handle("/metrics", promhttp.Handler())
 
 	// 4. LA NOUVELLE ROUTE WEBSOCKET
-	mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
-		// On autorise toutes les origines pour le développement (CORS)
-		ws.ServeWs(hub, w, r)
-	})
+	// mux.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+	// 	// On autorise toutes les origines pour le développement (CORS)
+	// 	ws.ServeWs(hub, w, r)
+	// })
+	mux.Handle("/ws", metrics.TrackRequests(metrics.TrackActiveUsers(metrics.TrackVisitDuration(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        ws.ServeWs(hub, w, r)
+    })))))
+
+	handler := metrics.TrackRequests(metrics.TrackUniqueVisitors(mux))
 
 	log.Println("Serveur sur http://localhost:8081")
-	log.Fatal(http.ListenAndServe(":8081", metrics.TrackRequests(mux)))
+	// log.Fatal(http.ListenAndServe(":8081", metrics.TrackRequests(mux)))
+	log.Fatal(http.ListenAndServe(":8081", handler))
 }
