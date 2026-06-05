b84743706f6ea4fe30046ff5b3d685177b9edba2
Obergod <loupmael19@gmail.com>
2026-06-05 17:18:33 +0200

    merge with matti

--- internal/game/init.go
+++ internal/game/init.go
@@ -16,13 +16,13 @@ func InitGame(nbPlayer int) (*world.World, []string) {
 	var localIDs []string
 
     // joueur 1
-    p1 := player.NewPlayer(fixed.I(400), fixed.I(300), 100, "p1")
+    p1 := player.NewPlayer(fixed.I(600), fixed.I(360), 100, "p1")
     w.AddPlayer(p1.ID, p1)
 	localIDs = append(localIDs, p1.ID)
 
     // jia add un if tout con si on appuie bien sur 2 joueur
     if nbPlayer == 2 {
-		p2 := player.NewPlayer(fixed.I(450), fixed.I(300), 100, "p2")
+		p2 := player.NewPlayer(fixed.I(680), fixed.I(360), 100, "p2")
         w.AddPlayer(p2.ID, p2)
 		localIDs = append(localIDs, p2.ID)
 	}
